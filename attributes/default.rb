f56da975b0e0753581a75d34f67927c2b451f467
Kirillov Alexander <saratovsource@gmail.com>
2013-11-14 18:39:32 +0400

    Change names

--- attributes/default.rb
+++ attributes/default.rb
@@ -54,5 +54,5 @@ default['transmission']['speed_limit_down_enabled'] = 'false'
 default['transmission']['speed_limit_up']           = 100 #KB/s
 default['transmission']['speed_limit_up_enabled']   = 'false'
 
-default['transmission']['rpc-whitelist']          = '127.0.0.1'
-default['transmission']['rpc-whitelist-enabled']  = 'true'
+default['transmission']['rpc_whitelist']          = '127.0.0.1'
+default['transmission']['rpc_whitelist_enabled']  = 'true'
