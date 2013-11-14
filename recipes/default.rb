b6e9320ba129fc3d1804f11b4da1733ed2a36763
Kirillov Alexander <saratovsource@gmail.com>
2013-11-14 16:15:37 +0400

    Other default config path

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,7 +34,7 @@ require 'transmission-simple'
 
 template "transmission-default" do
   case node['platform']
-  when "centos", "redhat", "amazon", "scientific", "raspbian"
+  when "centos", "redhat", "amazon", "scientific"
     path "/etc/sysconfig/transmission-daemon"
   else
     path "/etc/default/transmission-daemon"
