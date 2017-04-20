787eaa461f5b52a7d0ba6e7e1bfd39122dfdb7fd
Tim Smith <tsmith@chef.io>
2017-04-20 15:34:07 -0700

    Add suse platforms to the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs and configures transmission'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.0.0'
 
-%w(ubuntu debian redhat centos suse scientific oracle amazon fedora).each do |os|
+%w(ubuntu debian redhat centos suse scientific oracle amazon fedora opensuse opensuseleap).each do |os|
   supports os
 end
 
