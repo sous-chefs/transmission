d767f9d10bcfcdf96b0e255ccde6a719ecc7b210
Jamie Tanna <jamie@jamietanna.co.uk>
2017-10-31 08:16:25 +0000

    Add initial test to confirm attributes correctly set
    As part of #20.

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,19 +1,35 @@
 require 'spec_helper'
 
-describe 'package recipe on ubuntu 16.04' do
-  let(:runner) { ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04') }
-  let(:chef_run) { runner.converge('transmission::package') }
+describe 'transmission::default' do
+  context 'When running on a Debian platform' do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new(platform: 'debian', version: '8.0')
+      runner.converge(described_recipe)
+    end
 
-  it 'converges successfully' do
-    expect { chef_run }.to_not raise_error
+    it 'sets default install_method to package' do
+      expect(chef_run.node['transmission']['install_method']).to eq 'package'
+    end
+
+    it 'sets default user and group' do
+      expect(chef_run.node['transmission']['user']).to eq 'debian-transmission'
+      expect(chef_run.node['transmission']['group']).to eq 'debian-transmission'
+    end
   end
-end
 
-describe 'source recipe on ubuntu 16.04' do
-  let(:runner) { ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04') }
-  let(:chef_run) { runner.converge('transmission::source') }
+  context 'When running on a non-Debian platform' do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new(platform: 'centos', version: '7.2.1511')
+      runner.converge(described_recipe)
+    end
 
-  it 'converges successfully' do
-    expect { chef_run }.to_not raise_error
+    it 'sets default install_method to package' do
+      expect(chef_run.node['transmission']['install_method']).to eq 'source'
+    end
+
+    it 'sets default user and group' do
+      expect(chef_run.node['transmission']['user']).to eq 'transmission'
+      expect(chef_run.node['transmission']['group']).to eq 'transmission'
+    end
   end
 end
