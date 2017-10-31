a7a746dcc287df488a727176e304f66ce66fa7eb
Jamie Tanna <jamie@jamietanna.co.uk>
2017-10-31 09:03:49 +0000

    Ensure `include_recipe`s are matched

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -43,4 +43,46 @@ describe 'transmission::default' do
       expect(chef_run).to run_ruby_block('require transmission-simple gem')
     end
   end
+
+  context 'When install_method is from source' do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new(platform: 'centos', version: '7.2.1511') do |node|
+        node.automatic['transmission']['install_method'] = 'source'
+      end
+      runner.converge(described_recipe)
+    end
+
+    before :each do
+      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe)
+        .and_raise('include_recipe not matched')
+      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe)
+        .with('transmission::source')
+    end
+
+    it 'includes the transmission::package recipe' do
+      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('transmission::source')
+      chef_run
+    end
+  end
+
+  context 'When install_method is from package' do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new(platform: 'debian', version: '8.0') do |node|
+        node.automatic['transmission']['install_method'] = 'package'
+      end
+      runner.converge(described_recipe)
+    end
+
+    before :each do
+      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe)
+        .and_raise('include_recipe not matched')
+      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe)
+        .with('transmission::package')
+    end
+
+    it 'includes the transmission::package recipe' do
+      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('transmission::package')
+      chef_run
+    end
+  end
 end
