c42dbb02edd2f064785daac135fd5a6398624550
Tim Smith <tsmith84@gmail.com>
2019-11-10 18:15:51 -0800

    Remove EOL long_description metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs and configures transmission'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.0.1'
 
 %w(ubuntu debian redhat centos suse scientific oracle amazon fedora opensuse opensuseleap).each do |os|
